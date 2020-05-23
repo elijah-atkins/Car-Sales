c9ab02617403b05abf3961b2c38881c22f73f447
elijah-atkins <elijahorama@gmail.com>
2020-05-23 07:05:41 -0600

    commenting code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import createStore from redux
+// import provider from react-reduce
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+// import reducer
+
+
+
+// declare state
+// use createStore and pass in reducer
 
 const rootElement = document.getElementById('root');
+//wrap App with Provider tag
 ReactDOM.render(<App />, rootElement);
