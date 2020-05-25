f18926040670d98b330b3d921e85e81e2fda3aeb
elijah-atkins <elijahorama@gmail.com>
2020-05-24 18:03:45 -0600

    more planing

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -1,4 +1,11 @@
 // declare variables
+export const ADD_FEATURE = "ADD_FEATURE";
+export const REMOVE_FEATURE = "REMOVE_FEATURE";
 
 
 //actions
+
+// export const addFeature = id => ({
+//     type: ADD_FEATURE,
+//     payload: { id }
+// })
